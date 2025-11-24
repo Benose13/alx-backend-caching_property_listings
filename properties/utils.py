048255f681cf36db93091c6b872693ea45bfc862
jaymine <okoegualebenjamin38@gmail.com>
2025-11-24 04:41:18 +0100

    Cache Property List View

---
+++ properties/utils.py
@@ -0,0 +1,22 @@
+from django.core.cache import cache
+from .models import Property
+
+def get_all_properties():
+    """
+    Get all properties from cache if available, otherwise fetch from database
+    and cache for 1 hour (3600 seconds)
+    """
+    # Try to get properties from cache
+    properties = cache.get('all_properties')
+    
+    if properties is None:
+        # If not in cache, fetch from database
+        properties = Property.objects.all()
+        
+        # Store in cache for 1 hour (3600 seconds)
+        cache.set('all_properties', properties, 3600)
+        print("Properties fetched from database and cached")
+    else:
+        print("Properties fetched from cache")
+    
+    return properties
--- properties/views.py
+++ properties/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.cache import cache_page
 from .models import Property
+from .utils import get_all_properties
 
 # Cache the view for 15 minutes (60 * 15 seconds)
 @cache_page(60 * 15)
@@ -10,4 +11,7 @@ def property_list(request):
     properties = Property.objects.all().values(
         "id", "title", "description", "price", "location", "created_at"
     )
-    return JsonResponse(list(properties), safe=False)
+    data = {
+        "data": list(properties)
+    }
+    return JsonResponse(data)
